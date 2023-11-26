7e646cb00c9dc85086d972ea72e577727b2f2896
Zhaleh Jamalivand <zhaleh.jamalivand@gmail.com>
2023-11-26 23:12:18 +0330

    Commit Calculation toll based on each hour max amount

--- Commands/CongestionTaxCalculator.cs
+++ Commands/CongestionTaxCalculator.cs
@@ -30,42 +30,46 @@ namespace congestion.calculator.Commands
         {
             int maxTollAmountPerDay = infoOfDayToll.MaxTollFeeOfEveryDay;
             int totalFee = 0;
-            List<(DateTime entranceTime, int fee)> selectedEnteranceAmount = CalculateTheListOFPayableToll(vehicle, dates);
+            List<(DateTime entranceTime, int fee)> selectedEnterance = CalculateTheListOFPayableToll(vehicle, dates);
+            
             //TODO: Problem about midnight hour and entrances
-            totalFee = selectedEnteranceAmount.GroupBy(e => e.entranceTime.Date)
-                                              .Select(w => new { w.Key.Date, dayTaxAmount = w.Sum(e => e.fee) })
-                                              .Sum(e => e.dayTaxAmount > maxTollAmountPerDay ? maxTollAmountPerDay : e.dayTaxAmount);
+            
+            totalFee = selectedEnterance.GroupBy(e => e.entranceTime.Date)
+                                        .Select(w => new { w.Key.Date, dayTaxAmount = w.Sum(e => e.fee) })
+                                        .Sum(e => e.dayTaxAmount > maxTollAmountPerDay ? maxTollAmountPerDay : e.dayTaxAmount);
             return totalFee;
         }
 
         private List<(DateTime entranceTime, int fee)> CalculateTheListOFPayableToll(Vehicle vehicle, DateTime[] dates)
         {
-            List<(DateTime entranceTime, int fee)> allEnteranceFee = new();
             List<(DateTime entranceTime, int fee)> selectedEnteranceAmount = new();
+
             var orderedDateTime = dates.OrderBy(dt => dt).ToList();
             for (int i = 0; i < orderedDateTime.Count(); i++)
             {
                 (DateTime entranceTime, int fee) newEntranceTollFee = (orderedDateTime[i], infoOfDayToll.GetTollFee(orderedDateTime[i], vehicle));
-                allEnteranceFee.Add(newEntranceTollFee);
 
-                selectedEnteranceAmount.Add(CalculateMaxTollFeeInLastHour(allEnteranceFee));
+                selectedEnteranceAmount.Add(newEntranceTollFee);
+                selectedEnteranceAmount = ReFillListBasedNewEntrance(selectedEnteranceAmount, newEntranceTollFee.entranceTime);
             }
             return selectedEnteranceAmount;
         }
-
-        private (DateTime entranceTime, int fee) CalculateMaxTollFeeInLastHour(List<(DateTime entranceTime, int fee)> allEnteranceFee)
+         
+        private List<(DateTime entranceTime, int fee)> ReFillListBasedNewEntrance(List<(DateTime entranceTime, int fee)> listOFEnteranceAmount,
+                                                                                  DateTime newEntranceDate)
         {
-            //TODO: Change to the function with flexiable time getting from DB
-            int previuseEnterance = allEnteranceFee.Count-1;
-            (DateTime entranceTime, int fee) MaxEnteranceAmount = allEnteranceFee.Last();
+            List<(DateTime entranceTime, int fee)> EnteranceAmount = listOFEnteranceAmount;
+            var listOfLastHourEntrances = listOFEnteranceAmount.Where(e => (newEntranceDate - e.entranceTime).TotalMinutes < 60)
+                                                               .OrderBy(e => e.fee)
+                                                               .ToList();
 
-            while (previuseEnterance >= 0 && (MaxEnteranceAmount.entranceTime - allEnteranceFee[previuseEnterance].entranceTime).TotalMinutes <= 60)
+            if (listOfLastHourEntrances.Count() != 1)
             {
-                if (MaxEnteranceAmount.fee <= allEnteranceFee[previuseEnterance].fee)
-                    MaxEnteranceAmount = allEnteranceFee[previuseEnterance];
-                previuseEnterance--;
+                var maxone = listOfLastHourEntrances.Last();
+                EnteranceAmount.RemoveAll(r => listOfLastHourEntrances.Any(e => e.Equals(r)));
+                EnteranceAmount.Add(maxone);
             }
-            return MaxEnteranceAmount;
+            return EnteranceAmount;
         }
     }
 }
